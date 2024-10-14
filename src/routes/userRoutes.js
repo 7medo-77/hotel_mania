8785db48a47c95b6b2aff003d252fc4db4ecff3b
hmedo <hmedo@7medo>
2024-10-14 06:02:22 +0300

    added validation class, created user POST route, NOT TESTED

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
+const validateClass = require('../middleware/validatorClass');
+const userController = require('../controllers/userController');
 
 const router = Router();
 
-router.post('/signup', (req, res) => {
+router.post('/signup', validateClass.validateUser(req, res), (req, res) => {
+  userController.addCurrentValidatedUser(req, res);
 });
 
 router.post('/login', (req, res) => {
