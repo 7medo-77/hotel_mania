c7282c48bf4e218e264af33fc7a5b8211580589b
hmedo <hmedo@7medo>
2024-10-16 13:08:02 +0300

    improved seeding script with reservations, doing reviews now

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 // import { express } from 'express';
 const express = require('express');
+const cors = require('cors');
 const allRouter = require('./routes/index');
 
 const app = express();
 
 app.set('json spaces', 2);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
