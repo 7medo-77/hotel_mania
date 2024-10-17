8b1c490eb2a94732aed223b77dc63c978db9bcc6
hmedo <hmedo@7medo>
2024-10-17 13:26:13 +0300

    reset repo

--- src/components/menuGetter/menuGetter.jsx
+++
@@ -1,8 +0,0 @@
-
-import { getMenu } from '@/lib/action';
-import Order from '../order/order';
-export default async function MenuGetter() {
-  const menu = await getMenu();
-  return(<Order menu={menu}/>
-  )
-}
